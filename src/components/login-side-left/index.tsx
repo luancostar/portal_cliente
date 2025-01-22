a4460ce4429b9b3f89c3bc3e3f408d7d837e5d82
Luan <luancostar@gmail.com>
2025-01-22 14:37:25 -0300

    Update home

--- src/components/login-side-left/index.tsx
+++ src/components/login-side-left/index.tsx
@@ -156,6 +156,7 @@ export default function FormLogin() {
             fullWidth
             onClick={handleClick}
             disabled={loading}
+          
           >
             {loading ? "Carregando..." : "LOGIN"}
           </Button>
