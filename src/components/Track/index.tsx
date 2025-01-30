7d67d1f4d2bc63a1db29ed671283584ebba0547b
Luan <luancostar@gmail.com>
2025-01-30 18:30:46 -0300

    portal cliente

--- src/components/Track/index.tsx
+++ src/components/Track/index.tsx
@@ -1,80 +1,111 @@
-import {
-  Card,
-  CardBody,
-} from "@material-tailwind/react";
-import { FaDoorOpen } from "react-icons/fa";
+import { useState } from "react";  // Importa o hook useState
+import { useNavigate } from "react-router-dom";  // Importa useNavigate
+import { Card, CardBody } from "@material-tailwind/react";
+import { FaDoorOpen, FaSpinner } from "react-icons/fa";
 import { FaTruckRampBox, FaBoxesStacked, FaRegPaperPlane } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 
 export default function TrackTurn() {
+  const navigate = useNavigate();  // Inicializa o hook useNavigate
+  const [loading, setLoading] = useState(false);  // Estado para controlar o loading
+
+  // Função para limpar o localStorage, exibir o loading e redirecionar
+  const handleLogout = () => {
+    setLoading(true);  // Ativa o loading
+    localStorage.clear();  // Limpa o localStorage
+
+    // Adiciona um delay de 2 segundos antes de redirecionar
+    setTimeout(() => {
+      setLoading(false);  // Desativa o loading
+      navigate("/");  // Redireciona para a página de login
+    }, 1500);  // Delay de 2 segundos
+  };
+
   return (
-    <Card className="mt-4 shadow-none w-full">
-      <CardBody
-        style={{ backgroundColor: "#fff", borderRadius: "10px" }}
-        className="w-full flex flex-col gap-4"
-      >
-        <div className="w-full justify-between flex sm:justify-around">
-          {/* Nova Coleta */}
-          <div className="grid justify-items-center">
-          <Link to="/newcollect" className="flex flex-col items-center group">
-            <div
-              style={{
-                borderRadius: "100%",
-              }}
-              className="w-fit cursor-pointer bg-gradient-to-b from-green-500 to-green-700 group-hover:from-white group-hover:to-white group-hover:border group-hover:border-green-500 transform transition-transform duration-200 group-hover:scale-95"
-            >
-              <FaTruckRampBox className="text-6xl p-3 text-white group-hover:text-green-500" />
+    <div >
+      {/* Exibe o loading em tela cheia */}
+      {loading && (
+        <div className="absolute inset-0 flex justify-center items-center bg-gray-800 bg-opacity-70 z-50">
+          <div className="text-white text-xl">
+            <div className="animate-spin flex justify-center items-center">
+              <FaSpinner className="text-6xl text-white" />
             </div>
-            <label className="text-xs font-bold text-center">Nova Coleta</label>
-          </Link>
-        </div>
-
-
-          {/* Minhas Coletas */}
-          <div className="grid justify-items-center">
-            <Link to="/mycollects" className="flex flex-col items-center group">
-              <div
-                style={{
-                  borderRadius: "100%",
-                }}
-                className="w-fit cursor-pointer bg-gradient-to-b from-green-500 to-green-700 group-hover:from-white group-hover:to-white group-hover:border group-hover:border-green-500 transform transition-transform duration-200 group-hover:scale-95"
-              >
-                <FaBoxesStacked className="text-6xl p-3 text-white group-hover:text-green-500" />
-              </div>
-              <label className="text-xs font-bold text-center"> Coletas</label>
-            </Link>
-          </div>
-
-          {/* Suporte */}
-          <div className="grid justify-items-center">
-            <Link to="/support" className="flex flex-col items-center group">
-              <div
-                style={{
-                  borderRadius: "100%",
-                }}
-                className="w-fit cursor-pointer bg-gradient-to-b from-green-500 to-green-700 group-hover:from-white group-hover:to-white group-hover:border group-hover:border-green-500 transform transition-transform duration-200 group-hover:scale-95"
-              >
-                <FaRegPaperPlane className="text-6xl p-3 text-white group-hover:text-green-500" />
-              </div>
-              <label className="text-xs font-bold text-center">Fala <strong>VB</strong></label>
-            </Link>
-          </div>
-          <div className="grid justify-items-center">
-            <Link to="/support" className="flex flex-col items-center group">
-              <div
-                style={{
-                  borderRadius: "100%",
-                }}
-                className="w-fit cursor-pointer bg-gradient-to-b from-white to-gray-600 group-hover:from-white group-hover:to-white group-hover:border group-hover:border-green-500 transform transition-transform duration-200 group-hover:scale-95"
-              >
-                <FaDoorOpen className="text-6xl p-3 text-white group-hover:text-green-500" />
-              </div>
-              <label className="text-xs font-bold text-center">Sair </label>
-            </Link>
+            <div>Deslogando...</div>
           </div>
         </div>
- 
-      </CardBody>
-    </Card>
+      )}
+
+      <Card className="mt-4 shadow-none w-full">
+        <CardBody
+          style={{ backgroundColor: "#fff", borderRadius: "10px" }}
+          className="w-full flex flex-col gap-4"
+        >
+          <div className="w-full justify-between flex sm:justify-around">
+            {/* Nova Coleta */}
+            <div className="grid justify-items-center">
+              <Link to="/newcollect" className="flex flex-col items-center group">
+                <div
+                  style={{
+                    borderRadius: "100%",
+                  }}
+                  className="w-fit cursor-pointer bg-gradient-to-b from-green-500 to-green-700 group-hover:from-white group-hover:to-white group-hover:border group-hover:border-green-500 transform transition-transform duration-200 group-hover:scale-95"
+                >
+                  <FaTruckRampBox className="text-6xl p-3 text-white group-hover:text-green-500" />
+                </div>
+                <label className="text-xs font-bold text-center">Nova Coleta</label>
+              </Link>
+            </div>
+
+            {/* Minhas Coletas */}
+            <div className="grid justify-items-center">
+              <Link to="/mycollects" className="flex flex-col items-center group">
+                <div
+                  style={{
+                    borderRadius: "100%",
+                  }}
+                  className="w-fit cursor-pointer bg-gradient-to-b from-green-500 to-green-700 group-hover:from-white group-hover:to-white group-hover:border group-hover:border-green-500 transform transition-transform duration-200 group-hover:scale-95"
+                >
+                  <FaBoxesStacked className="text-6xl p-3 text-white group-hover:text-green-500" />
+                </div>
+                <label className="text-xs font-bold text-center"> Coletas</label>
+              </Link>
+            </div>
+
+            {/* Suporte */}
+            <div className="grid justify-items-center">
+              <Link to="/support" className="flex flex-col items-center group">
+                <div
+                  style={{
+                    borderRadius: "100%",
+                  }}
+                  className="w-fit cursor-pointer bg-gradient-to-b from-green-500 to-green-700 group-hover:from-white group-hover:to-white group-hover:border group-hover:border-green-500 transform transition-transform duration-200 group-hover:scale-95"
+                >
+                  <FaRegPaperPlane className="text-6xl p-3 text-white group-hover:text-green-500" />
+                </div>
+                <label className="text-xs font-bold text-center">Fala <strong>VB</strong></label>
+              </Link>
+            </div>
+
+            {/* Sair */}
+            <div className="grid justify-items-center">
+              <button
+                onClick={handleLogout}  // Chama a função handleLogout ao clicar
+                className="flex flex-col items-center group"
+              >
+                <div
+                  style={{
+                    borderRadius: "100%",
+                  }}
+                  className="w-fit cursor-pointer bg-gradient-to-b from-white to-gray-600 group-hover:from-white group-hover:to-white group-hover:border group-hover:border-green-500 transform transition-transform duration-200 group-hover:scale-95"
+                >
+                  <FaDoorOpen className="text-6xl p-3 text-white group-hover:text-green-500" />
+                </div>
+                <label className="text-xs font-bold text-center">Sair</label>
+              </button>
+            </div>
+          </div>
+        </CardBody>
+      </Card>
+    </div>
   );
 }
