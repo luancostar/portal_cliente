9a3aa0837a16c093e171d997146552fb235fdf1c
Luan <luancostar@gmail.com>
2025-02-12 11:53:11 -0300

    Up

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -7,7 +7,7 @@ import CoverLogin from "../../components/login-side-right";
 export default function Login() {
  return (
    <div style={{ overflow: 'hidden' }}  className="h-full flex justify-center">
-{/* <FormLogin/> */}
+<FormLogin/>
 <CoverLogin/>
    </div>
  );
